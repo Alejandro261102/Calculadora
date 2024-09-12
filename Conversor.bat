61b09850a8ed03f3e58cb6737c7bce8774866da1
OswaldoGV27 <oswaldogil2im10@gmail.com>
2024-09-12 11:04:21 -0600

    Actualizar Conversor.bat
    se agregó el comentario de los cambios realizados para la conversión de velocidad a flotante

--- Conversor.bat
+++ Conversor.bat
@@ -102,6 +102,10 @@ goto tiempo
     pause
     goto velocidad
 
+:: Se utiliza PowerShell para realizar el cálculo con números flotantes, (powershell -command "[math]::round()"). Y la función round() de PowerShell redondea el resultado a 2 decimales.
+
+Esta línea de comando se repite para cada tipo de conversión de velocidad, el resto del código se mantuvo de la forma original de los compañeros
+
 :kmhms
     set /p kmh="Ingresa la velocidad en Km/h: "
     for /f "tokens=*" %%A in ('powershell -command "[math]::round((%kmh%*1000/3600), 2)"') do set resultado=%%A
